0ed7e81b27b28e6508c0521fde962d8015550240
acdemeg <v.krylov1996@gmail.com>
2023-12-02 20:51:15 +0600

    [*] Fix_2 release_workflow.yml, fix .jar name

--- create_executable.bat
+++ create_executable.bat
@@ -1,10 +1,10 @@
-set JDEPS_COMMAND=jdeps --print-module-deps --ignore-missing-deps .\build\libs\Fifa19Bot-1.0.jar
+set JDEPS_COMMAND=jdeps --print-module-deps --ignore-missing-deps .\build\libs\FifaBot.jar
 @rem set list needs modules
 for /f "delims=" %%i in ('%JDEPS_COMMAND%') do set MODULES=%%i
 @rem create minimal runtime
 jlink --add-modules %MODULES% --output .\build\AIbotRuntime
 @rem move .jar in AIbotRuntime
-cp .\build\libs\Fifa19Bot-1.0.jar .\build\AIbotRuntime
+cp .\build\libs\FifaBot.jar .\build\AIbotRuntime
 @rem create executable file
 cd build
-npx caxa --input .\AIbotRuntime --output FifaBot.exe --no-include-node -- "{{caxa}}/bin/java" "-jar" "{{caxa}}/FifaBot-1.0.jar"
+npx caxa --input .\AIbotRuntime --output FifaBot.exe --no-include-node -- "{{caxa}}/bin/java" "-jar" "{{caxa}}/FifaBot.jar"
